5f8ccd2d63795d3e6528bddd82837a1abcf46d26
Mustafizur Reza <mustafizurreza80@gmail.com>
2021-03-22 23:48:02 +0600

    budget

--- assignment.js
+++ assignment.js
@@ -1,7 +1,7 @@
 //kilometerToMeter
 function kilometerToMeter(distance){
     if(distance<0){
-        return "Negative is not allowed";
+        return "Negative value is not allowed";
     }
     else{
         var meter = distance*1000;
@@ -9,4 +9,17 @@ function kilometerToMeter(distance){
     }
 }
 var result = kilometerToMeter(10);
-console.log(result);
+console.log(result1);
+
+//budgetCalculator
+function budgetCalculator(watch,phone,laptop){
+    if(watch<0 || phone<0 || laptop<0){
+        return "Negative value is not allowed";
+    }
+    else{
+        var totalcost = (watch*50)+(phone*100)+(laptop*500);
+        return totalcost;
+    }
+}
+var result2 = budgetCalculator(8,9,10);
+console.log(result2);
