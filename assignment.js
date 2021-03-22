6b46c414bea513c4d1b2aa848198542ece7ae1e3
Mustafizur Reza <mustafizurreza80@gmail.com>
2021-03-22 23:58:07 +0600

    hotelCost

--- assignment.js
+++ assignment.js
@@ -8,7 +8,7 @@ function kilometerToMeter(distance){
         return meter;
     }
 }
-var result = kilometerToMeter(10);
+var result1 = kilometerToMeter(10);
 console.log(result1);
 
 //budgetCalculator
@@ -17,9 +17,37 @@ function budgetCalculator(watch,phone,laptop){
         return "Negative value is not allowed";
     }
     else{
-        var totalcost = (watch*50)+(phone*100)+(laptop*500);
+        var totalcost = (watch*50) + (phone*100) + (laptop*500);
         return totalcost;
     }
 }
 var result2 = budgetCalculator(8,9,10);
 console.log(result2);
+
+//hotelCost
+function hotelCost(days){
+    var PackageOne = 100;
+    var PackageTwo = 80;
+    var PackageThree = 50;
+    if(days<0){
+        return "Negative value is not allowed";
+    }
+    if(days>=1 && days<=10){
+        var package1 = days*PackageOne;
+        return package1;
+    }
+    if(days>=11 && days<=20){
+        var remainValue1 = days-10;
+        var p2 = remainValue1*PackageTwo;
+        var package2 = p2+(10*PackageOne);
+        return package2;
+    }
+    if(days>=21){
+        var remainValue2 = days-20;
+        var p3 = remainValue2*PackageThree;
+        var package3 = p3+(10*PackageOne)+(10*PackageTwo);
+        return package3;
+    }
+}
+var result3 = hotelCost(43);
+console.log(result3);
